20b489388bcbfece72eea760b7854ee53189d529
ngirimana <chadrackngirimana@gmail.com>
2022-02-03 11:56:37 +0200

    persist update task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -54,9 +54,10 @@ export class TasksService {
   //   return tasks;
   // }
 
-  // updateTaskStatus(id: string, status: TaskStatus) {
-  //   const task = this.getTaskById(id);
-  //   task.status = status;
-  //   return task;
-  // }
+  async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
+    const task = await this.getTaskById(id);
+    task.status = status;
+    await this.taskRepository.save(task);
+    return task;
+  }
 }
